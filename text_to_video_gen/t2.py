a7d65c283d00c9e2b369401c94cc0641541c84f8
jaeymax <azagojunior2@gmail.com>
2024-02-21 23:59:33 +0000

    added a requirments.txt file and resolved filepaths using the os module

--- text_to_video_gen/t2.py
+++ text_to_video_gen/t2.py
@@ -13,7 +13,7 @@ import pygame as pg
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-
+import os
 
 
 #First request the AI to generate the vertices of the objects in the given scene.
@@ -26,7 +26,14 @@ draw = CustomDraw()
 # scene.addObject("cube",so)
 # scene.addObject("gun",gun)
 
-obj = util.read_object_data(r"objectFiles\objectFile.txt")
+objectFile_path = os.path.join("./objectFiles","objectFile.txt")
+
+
+#obj = util.read_object_data(r"objectFiles\objectFile.txt")
+
+obj = util.read_object_data(objectFile_path)
+
+
 scene.addObject(obj.name,obj)
 
 # obj util.read_object_data("")
@@ -35,7 +42,11 @@ scene.addObject(obj.name,obj)
 if __name__ == "__main__":
     # main()
     # scene.playScene(r"animationFiles\triangleAnim.txt",20,30)
-    scene.playScene(r"animationFiles\positions.txt",40,10)
+    #scene.playScene(r"animationFiles\positions.txt",40,10)
+    #pt = os.path.join("./animationFiles","positions.txt")
+    animationFile_path = os.path.join("./animationFiles","positions.txt")
+    
+    scene.playScene(animationFile_path, 40,10)
 
 
 
